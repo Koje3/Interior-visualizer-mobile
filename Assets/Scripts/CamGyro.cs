db66c27d18e061ddd4162e9640edbb238df0f9ba
Koje3 <91778162+Koje3@users.noreply.github.com>
2021-11-22 23:43:00 +0200

    Made camera position change system

--- Assets/Scripts/CamGyro.cs
+++ Assets/Scripts/CamGyro.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 
 public class CamGyro : MonoBehaviour
 {
+    public Camera arCamera;
+    public GameObject cameraPositions;
+    public GameObject startPosition;
+    public GameObject ceilingLights;
+    public GameObject roof;
+    public GameObject camera2DPosition;
+
     private bool gyroEnabled;
     private Gyroscope gyro;
 
     private GameObject cameraContainer;
     private Quaternion rot;
 
+    private GameObject hitObject;
 
-    public GameObject[] cameraPositions;
+    private GameObject currentCameraPosition;
+    private GameObject previousCameraPosition;
+
+    private bool camera2D;
+
+
+   // public GameObject[] cameraPositions;
 
     private void Start()
     {
@@ -21,6 +35,13 @@ public class CamGyro : MonoBehaviour
         transform.SetParent(cameraContainer.transform);
 
         gyroEnabled = EnableGyro();
+       
+        currentCameraPosition = startPosition;
+        startPosition.SetActive(false);
+
+        roof.SetActive(true);
+
+        camera2D = false;
 
     }
 
@@ -44,12 +65,35 @@ public class CamGyro : MonoBehaviour
         {
             transform.localRotation = gyro.attitude * rot;
         }
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                RaycastChangePosition(touch);
+            }
+
+            if (camera2D == true)
+            {
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    MoveCamera2D(touch);
+                }
+
+                Touch touch2 = Input.GetTouch(1);
+
+            }
+        }
     }
 
+
+    /*
     public void ChangePosition()
     {
-        Vector3 position1 = cameraPositions[1].transform.position;
-        Vector3 position2 = cameraPositions[2].transform.position;
+        Vector3 position1 = cameraPositions[0].transform.position;
+        Vector3 position2 = cameraPositions[1].transform.position;
 
         if (cameraContainer.transform.position == position1)
         {
@@ -62,5 +106,95 @@ public class CamGyro : MonoBehaviour
         }
 
     }
+    */
 
+    public void RaycastChangePosition(Touch touch)
+    {
+        Ray ray = arCamera.ScreenPointToRay(touch.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            hitObject = hit.transform.gameObject;
+
+            Debug.Log(hitObject);
+
+            if (hitObject.tag == "Camera Position")
+            {
+
+                if (camera2D == true)
+                {
+                    arCamera.orthographic = false;
+                    gyroEnabled = true;
+                    camera2D = false;
+                }
+
+                previousCameraPosition = currentCameraPosition;
+                currentCameraPosition = hitObject;               
+
+                cameraContainer.transform.position = hitObject.transform.position;
+
+                previousCameraPosition.SetActive(true);
+                currentCameraPosition.SetActive(false);
+
+            }
+        }
+    }
+
+    public void ShowCameraPositions()
+    {
+        if (cameraPositions.activeSelf)
+        {
+            cameraPositions.SetActive(false);
+        }
+        else if (!cameraPositions.activeSelf)
+        {
+            cameraPositions.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Roof")
+        {
+            roof.SetActive(false);
+            ceilingLights.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Roof")
+        {
+            roof.SetActive(true);
+            ceilingLights.SetActive(true);
+        }
+    }
+
+    public void Camera2D()
+    {
+        if (cameraContainer.transform.position != camera2DPosition.transform.position)
+        {
+            camera2D = true;
+
+            previousCameraPosition = currentCameraPosition;
+            currentCameraPosition = camera2DPosition;
+
+            cameraContainer.transform.position = camera2DPosition.transform.position;
+
+            previousCameraPosition.SetActive(true);
+            currentCameraPosition.SetActive(false);
+
+            arCamera.orthographic = true;
+            gyroEnabled = false;
+
+            transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+
+        }
+    }
+
+    public void MoveCamera2D(Touch touch)
+    {
+
+    }
 }
